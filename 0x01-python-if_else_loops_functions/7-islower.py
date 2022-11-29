6ea62935b557aaaf16cbcb13f556fdc25048b2f7
dgikonyo <denniskimani05@gmail.com>
2022-11-29 23:06:50 +0300

    task

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author - Gikonyo Kimani
+
+def isLower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author - Gikonyo Kimani
+
+def uppercase(str):
+    if ord(str) >= 65 and ord(str) <=90:
+        return True
+    else:
+        return False
