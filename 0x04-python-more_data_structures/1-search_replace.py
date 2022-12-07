aba9da1ede68348f0cb0e60dda850b3a4e6c0263
dgikonyo <denniskimani05@gmail.com>
2022-12-07 22:15:22 +0300

    unique add

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     copy = []
-    low = 0
-    high = len(my_list) -1
-
-    while low <= high:
-        middle = (low + (high - low)) / 2
-
-        if my_list[middle] == search:
+    for i in range(len(my_list)):
+        if my_list[i] == search:
             copy.append(replace)
-        elif my_list[middle] < search:
-            copy.append(my_list[low])
-            low = middle + 1
         else:
-            copy.append(my_list[high])
-            high = middle -1
+            copy.append(my_list[i])
     return copy
