9054957fc75b14a4b4c5296454ec7eb408662ee4
dgikonyo <dennis.gikonyo@jubileekenya.com>
2022-12-07 10:43:50 +0300

    some changes

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -6,11 +6,20 @@
 #include <unistd.h>
 
 /**
- * struct listint_t - singly linked list
+ * struct listint_s - singly linked list
  * @head: points to the next node
  */
-struct node {
-	listint_t **head;
-};
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+void reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
 
 #endif
